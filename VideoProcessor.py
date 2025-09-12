3e302c2ebe7733293e6ac9bca67a9f945388f457
Naama Tzadok <naamatzadok10@gmail.com>
2025-09-12 10:21:02 +0300

    edit VideoProcessor.py

--- VideoProcessor.py
+++ VideoProcessor.py
@@ -1,62 +1,86 @@
 import numpy as np
-import cv2 
+import cv2 as cv
 
 
 class VideoProcessor:
     def __init__(self, path):
+        self.face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml') 
         self.path = path
+        self.frames = []
 
     # split the video into frames
     def process(self):
-        frames = []
-        video = cv2.VideoCapture(self.path)
+        video = cv.VideoCapture(self.path)
 
         if not video.isOpened():
             print("Error. video didn't open.")
         else:
-            totalFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-            while video.get(cv2.CAP_PROP_POS_FRAMES) <  totalFrames:
+            totalFrames = video.get(cv.CAP_PROP_FRAME_COUNT)
+            while video.get(cv.CAP_PROP_POS_FRAMES) <  totalFrames:
                 ret, frame = video.read()
 
                 if ret == False:
-                    print("frame ", video.get(cv2.CAP_PROP_FRAME_COUNT, " not read"))
+                    print("frame ", video.get(cv.CAP_PROP_FRAME_COUNT), " not read")
                     return None
+                
+                faces = self.face_detector(frame)
                 
                 # reduce rezolution to 100*100
                 # h, w = frame.shape[:2]
-                # temp = cv2.resize(frame, (100,100), interpolation= cv2.INTER_LINEAR)
-                # frame = cv2.resize(temp, (w, h), interpolation= cv2.INTER_LINEAR)
+                # temp = cv.resize(frame, (100,100), interpolation= cv.INTER_LINEAR)
+                # frame = cv.resize(temp, (w, h), interpolation= cv.INTER_LINEAR)
 
-                face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to gray scale                    
-                faces = face_cascade.detectMultiScale(grayFrame, scaleFactor=1.1, minNeighbors=5) # detact faces
-
-                if len(faces) > 1:
-                    print("There are more then one face in the frame. Try again.")
-                    continue
-                if len(faces) < 1:
-                    print("There is no face in the frame. Try again.")
-                    continue
+                # if len(faces) > 1:
+                #     print("There are more then one face in the frame. Try again.")
+                #     continue
+                # if len(faces) < 1:
+                #     print("There is no face in the frame. Try again.")
+                #     continue
                 
-                x, y, w, h = faces[0]
-                frame = frame[y:y+h, x:x+w]
-                if frame.size == 0:
-                    continue
-                frames.append(frame)
-        return frames
+                for (x, y, w, h) in faces:
+                    face = frame[y:y+h, x:x+w]
+                    if face.size == 0:
+                        continue
+                    self.frames.append(face)
+        return self.frames
+    
+    
+    def face_detector(self, image):
+        
+        
+        target_width = 1300
+        scale = image.shape[1]/target_width #the scale is the sorce width devided by the desierd with
+        target_hight = int(image.shape[0]/scale)
+        image = cv.resize(image, (target_width, target_hight), interpolation=cv.INTER_AREA)
+                
+        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)     # convert to gray scale for face haarcascade algoritem   
+
+        image = cv.equalizeHist(image)
+        
+        faces = self.face_cascade.detectMultiScale(
+            image, 
+            scaleFactor=1.1,       # סולם הפחתת גודל התמונה בכל מעבר
+            minNeighbors=5,        # מספר מינימלי של "שכנים" להכרזה על פנים
+            minSize=(30, 30)       # גודל מינימלי לחלון שבו ינסו לגלות פנים
+        )
+       
+        return faces
+    
     
     # returns number of frames in one second
     def framePerSec(self):
-        video = cv2.VideoCapture(self.path)
+        video = cv.VideoCapture(self.path)
         if not video.isOpened():
             print("Error. video didn't opened.")
         else:
-            return video.get(cv2.CAP_PROP_FPS)
+            return video.get(cv.CAP_PROP_FPS)
+    
         
 def main():
-        path = r"C:\Users\naama\Documents\studing\stress-level-analyzer\videos\naama_hand.mp4"
+        path = r"C:\Users\naama\Documents\studing\stress-level-analyzer\videos\WIN_20250905_18_04_10_Pro.mp4"
         video_processor = VideoProcessor(path)
         frames = video_processor.process()
+        
         if frames == None:
             return
 
@@ -67,9 +91,9 @@ def main():
         if not len(frames) == 0:
             i = 1
             for frame in frames:
-                cv2.imshow("frame "+str(i), frame)
-                cv2.waitKey(0) 
-                cv2.destroyAllWindows()
+                cv.imshow("frame "+str(i), frame)
+                cv.waitKey(0) 
+                cv.destroyAllWindows()
                 i += 1
 
 if __name__ == "__main__":
